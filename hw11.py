cb640db13c48b7dd284b741e149a156ae8b0e033
kraggyy <108898768+kraggyy@users.noreply.github.com>
2022-08-10 20:09:19 +0300

    Add files via upload

--- hw11.py
+++ hw11.py
@@ -15,17 +15,27 @@ class Auto(Vehicle):
     max_speed = 120
     size = 'small'
 
+    def get_info(self):
+        print(f'This vehicle could reach {self.max_speed} km/h')
 
 class Plane(Vehicle):
     max_speed = 800
     size = 'big'
 
+    def get_info(self):
+        print(f'This vehicle could reach {self.max_speed} km/h')
 
 class Ship(Vehicle):
     max_speed = 60
     size = 'enormous'
 
+    def get_info(self):
+        print(f'This vehicle could reach {self.max_speed} km/h')
 
 mersedes = Auto()
 boieng = Plane()
 MGS = Ship()
+
+print(mersedes.get_info())
+print(boieng.get_info())
+print(MGS.get_info())
